b3950e7dfcf6aafb39b1482656aec3b31f64de30
sa-ma <samailabalap@gmail.com>
2019-07-03 21:56:06 +0100

    chore(test) implement create admin test
    add test for create admin endpoint
    
    [Starts #166996197]

--- server/test/users.test.js
+++ server/test/users.test.js
@@ -184,4 +184,124 @@ describe('Test for Authentication Endpoints', () => {
         });
     });
   });
+
+  // Create Admin TESTS
+  describe(`POST ${url}admin`, () => {
+    // return 201 if admin created
+    it('should return 201 and successfully create a new admin', (done) => {
+      const user = {
+        email: 'aa@aa.aa',
+        firstName: 'Sama',
+        lastName: 'Bala',
+        password: '123456'
+      };
+      chai
+        .request(app)
+        .post(`${url}admin`)
+        .send(user)
+        .end((err, res) => {
+          res.should.have.status(201);
+          res.body.should.be.an('object');
+          res.body.should.have.property('status');
+          res.body.should.have.property('data');
+          res.body.data.should.be.an('object');
+          res.body.data.should.have.property('user_id').which.is.a('number');
+          res.body.data.should.have.property('is_admin').which.is.a('boolean').which.is.equal.to(true);
+          res.body.data.should.have.property('token').which.is.a('string');
+          done();
+        });
+    });
+    // return 400 if firstName is not provided
+    it('should return 400 if first name is not inserted', (done) => {
+      const user = {
+        lastName: 'Bala',
+        email: 'aa@aa.aa',
+        password: '123456'
+      };
+      chai
+        .request(app)
+        .post(`${url}admin`)
+        .send(user)
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.body.should.be.an('object');
+          res.body.should.have.property('error');
+          done();
+        });
+    });
+    // return 400 if lastName is not provided
+    it('should return 400 if last name is not inserted', (done) => {
+      const user = {
+        firstName: 'Bala',
+        email: 'aa@aa.aa',
+        password: '123456'
+      };
+      chai
+        .request(app)
+        .post(`${url}admin`)
+        .send(user)
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.body.should.be.an('object');
+          res.body.should.have.property('error');
+          done();
+        });
+    });
+    // return 400 if email is not provided
+    it('should return 400 if email is not inserted', (done) => {
+      const user = {
+        firstName: 'Sama',
+        lastName: 'Bala',
+        password: '123456'
+      };
+      chai
+        .request(app)
+        .post(`${url}admin`)
+        .send(user)
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.body.should.be.an('object');
+          res.body.should.have.property('error');
+          done();
+        });
+    });
+    // return 400 if password is not provided
+    it('should return 400 if password is not inserted', (done) => {
+      const user = {
+        email: 'aa@aa.aa',
+        firstName: 'Sama',
+        lastName: 'Bala',
+      };
+      chai
+        .request(app)
+        .post(`${url}admin`)
+        .send(user)
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.body.should.be.an('object');
+          res.body.should.have.property('error');
+          done();
+        });
+    });
+    // return 409 if email is taken
+    it('should return 409 if email is taken', (done) => {
+      const user = {
+        email: 'aa@aa.aa',
+        firstName: 'Sama',
+        lastName: 'Bala',
+        password: '123456'
+      };
+      chai
+        .request(app)
+        .post(`${url}admin`)
+        .send(user)
+        .end((err, res) => {
+          res.should.have.status(409);
+          res.body.should.be.an('object');
+          res.body.should.have.property('status');
+          res.body.should.have.property('error');
+          done();
+        });
+    });
+  });
 });
