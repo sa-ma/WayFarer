6951f1412b5e8ff76c40edd20fd4629821bb1050
Samaila Philemon Bala <samailabalap@gmail.com>
2019-07-04 00:48:31 +0100

    Revert "#166996197 Add create admin user endpoint"

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import UserController from '../controllers/userController';
 import validate from '../middlewares/validateInputs';
-import Authenticate from '../middlewares/auth';
 
 const router = express.Router();
 router.use(express.json());
@@ -15,10 +14,5 @@ router.post('/signup', validate.signUp, UserController.signUp);
 // @desc    Sign in Route
 // @access  Public
 router.post('/signin', validate.signIn, UserController.signIn);
-
-// @route   POST api/v1/auth/admin
-// @desc    Create Admin Route
-// @access  Private
-router.post('/admin', [Authenticate.verifyAdmin, validate.signUp], UserController.createAdmin);
 
 export default router;
