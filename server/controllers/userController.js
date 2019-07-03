8543266478d539d4bc32b80382551c84c879d821
sa-ma <samailabalap@gmail.com>
2019-07-03 23:51:04 +0100

    feature(endpoint) add create admin endpoint
    [Delivers #166996197]

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -53,5 +53,30 @@ class UserController {
     util.setSuccess(200, { user_id: user.id, is_admin: user.is_admin, token });
     return util.send(res);
   }
+
+  /**
+   * @method Create Admin
+   * @description Method to create an admin
+   * @param {object} req - The Request Object
+   * @param {object} res - The Response Object
+   * @returns {object} New Admin information
+   */
+  static async createAdmin(req, res) {
+    try {
+      const { rows } = await Users.createAdmin(req.body);
+      // eslint-disable-next-line camelcase
+      const { id, is_admin, } = rows[0];
+      const token = helper.generateToken({ id, is_admin });
+      util.setSuccess(201, { user_id: id, is_admin, token });
+      return util.send(res);
+    } catch (error) {
+      if (error.code === '23505') {
+        util.setError(409, 'Email already exists');
+        return util.send(res);
+      }
+      util.setError(500, 'Server Error');
+      return util.send(res);
+    }
+  }
 }
 export default UserController;
