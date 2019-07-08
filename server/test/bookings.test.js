cb1d4c497d7a34f9f88c8843eae90d509c77f27e
sa-ma <samailabalap@gmail.com>
2019-07-08 10:46:27 +0100

    chore(test) add view all booking endpoint test
    [Starts #167118907]

--- server/test/bookings.test.js
+++ server/test/bookings.test.js
@@ -136,4 +136,43 @@ describe('Test for Booking Endpoints', () => {
         });
     });
   });
+
+  // Get  all booking TESTS
+  describe(`GET ${bookingUrl}`, () => {
+    // return 200 if all trips are fetched
+    it('should return 200 and get all trips', (done) => {
+      const loginUser = {
+        email: 'admin@aa.aa',
+        password: '12345'
+      };
+      chai
+        .request(app)
+        .post(signinUrl)
+        .send(loginUser)
+        .end((autherr, authres) => {
+          const { token } = authres.body.data;
+          chai
+            .request(app)
+            .get(bookingUrl)
+            .set('x-auth-token', token)
+            .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.an('object');
+              res.body.should.have.property('status');
+              res.body.should.have.property('data');
+              res.body.data.should.be.an('array');
+              res.body.data[0].should.have.property('booking_id').which.is.a('number');
+              res.body.data[0].should.have.property('user_id').which.is.a('number');
+              res.body.data[0].should.have.property('trip_id').which.is.a('number');
+              res.body.data[0].should.have.property('bus_id').which.is.a('number');
+              res.body.data[0].should.have.property('trip_date').which.is.a('string');
+              res.body.data[0].should.have.property('seat_number').which.is.a('number');
+              res.body.data[0].should.have.property('first_name').which.is.a('string');
+              res.body.data[0].should.have.property('last_name').which.is.a('string');
+              res.body.data[0].should.have.property('email').which.is.a('string');
+              done();
+            });
+        });
+    });
+  });
 });
